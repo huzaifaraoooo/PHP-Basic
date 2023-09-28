86097d3619195b8fb3600f0e290d6ffc92bc7b24
huzaifaraoooo <137152876+huzaifaraoooo@users.noreply.github.com>
2023-09-28 10:29:37 -0700

    Add files via upload

---
+++ Newfile.php
@@ -0,0 +1,14 @@
+<?php
+$num1=4;
+$num2=5;
+
+echo "The  Sum of ". $num1."+". $num2." = ". $num1+$num2;
+echo"</br>";
+echo "The  Subtraction of ". $num1."-". $num2." = ". $num1-$num2;
+echo"</br>";
+echo "The  Division of ". $num1."/". $num2." = ". $num1/$num2;
+echo"</br>";
+echo "The  Multiplication of ". $num1."*". $num2." = ". $num1*$num2;
+echo"</br>";
+
+?>
